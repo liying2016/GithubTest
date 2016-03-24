c9d7c369578fa2a4aeb7b82d1d14c391a7c398fb
李营 <liying@liyingdeMacBook-Pro.local>
2016-03-24 09:27:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liying/githubtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.liying.githubtest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
